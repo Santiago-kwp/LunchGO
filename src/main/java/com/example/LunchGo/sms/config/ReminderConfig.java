baf786f5703e517af9c3045a53aa1ceb13be3ffb
김다혜 <56732608+Dahye021@users.noreply.github.com>
2026-01-10 20:24:59 +0900

    Delete src/main/java/com/example/LunchGo/sms/config/ReminderConfig.java

--- src/main/java/com/example/LunchGo/sms/config/ReminderConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.LunchGo.sms.config;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties(ReminderProperties.class)
-public class ReminderConfig {
-}
