20c4ee66f2e6673381c2a7b4c6bbf2e423f277b6
kwpark <pgw10243@gmail.com>
2026-01-28 19:14:28 +0900

    task: 윈도우 로컬 환경으로 마이그레이션 - kubernetes 환경 연동

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,13 +17,28 @@ export default defineConfig({
     },
   },
   server: {
-    host: '0.0.0.0',
+    // host: '127.0.0.1',
     port: 5173,
     allowedHosts: ['all'],
     proxy: {
+      // 이미지 경로로 시작하는 요청을 Nginx(80포트)로 보냄
+      '/restaurant': {
+        target: 'http://127.0.0.1:80/images', // /images를 붙여서 Nginx의 alias와 맞춤
+        changeOrigin: true,
+      },
+      // 2. 메뉴 이미지 (추가)
+      '/menus': {
+        target: 'http://127.0.0.1:80/images',
+        changeOrigin: true,
+      },
+      // 3. 리뷰 이미지 (추가)
+      '/reviews': {
+        target: 'http://127.0.0.1:80/images',
+        changeOrigin: true,
+      },
       // /api로 시작하는 요청은 백엔드 서버(8080)로 전달
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://127.0.0.1:8080',
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, '') // 필요시 경로 수정
       }
